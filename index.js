c859964f2a3791f310fcc1f59fa7be06e2e4616b
MohamedAtta22 <109322691+MohamedAtta22@users.noreply.github.com>
2025-07-06 00:39:19 +0300

    update index.js
    update index.js

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('DB connection error:', err));
 
 // Routes
 app.use('/api/auth', authRoutes);
