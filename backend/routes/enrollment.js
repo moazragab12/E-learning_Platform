d261325af3678cb575988696e8cf0b5f0a92f5bc
Onna03 <yumna55042771@gmail.com>
2025-07-17 03:41:15 +0300

    Delete all files from complete branch

--- backend/routes/enrollment.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/enrollmentController');
-
-router.post('/enroll', controller.enrollUser);
-router.get('/user/:userId', controller.getUserEnrollments);
-
-module.exports = router;
--- backend/routes/feedback.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/feedbackController.js');
-
-router.post('/', controller.submitFeedback);
-router.get('/course/:courseId', controller.getCourseFeedback);
-
-module.exports = router;
